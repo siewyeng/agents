d1dcaeda55eb6145c0943293cce093b48166c65b
Chua Chia Chiat <chia_chiat_chua@aiap.sg>
2023-10-13 17:25:03 +0800

    Added generated schedule

--- src/utils/generate_schedule.py
+++ src/utils/generate_schedule.py
@@ -1,3 +1,7 @@
+import os
+# from vertexai.preview.language_models import TextGenerationModel
+from langchain.llms import VertexAI
+import pandas as pd
 
 
 class Generate_Schedule:
@@ -38,6 +42,40 @@ class Generate_Schedule:
         ]
 
         return(schedule)
+
+    def generated_schedule(self,name:str,bio:str):
+        """
+        Generate the schedule for the agent base on the bio provided
+
+        Args:
+            name ([str]): [Name of the agent]
+            bio ([str]): [Bio description of the agent]
+
+        Returns:
+            [list]: [Schedule generated from LLM]
+        """
+
+
+
+        prompt = f"You are a plan generating AI, and your job is to help characters make new plans based on new information.\
+        Given the character's info(bio, goals), generate a new set of plans of the day for them to carry out, such that the final\
+        set of plans and include at least 15 individual plans. The plan\
+        list should be numbered in the order in which they should be performed, with each plan containing a description\
+        with at least 10 words and never use the word /'repeat/'\
+        Example plan: ['Plan to take dinner with my spouse at home']\
+        Try to collaborate with spouse if possible\
+        The location can only be office or home\
+        Do not mention about repeating or evaluating the plan\
+        Always prioritize finishing any pending conversation before doing other things.\
+        Let\'s Begin!\
+        Name: {name}\
+        Bio: {bio} \
+        \
+        "
+        # print(prompt)
+        llm = VertexAI(model_name='text-bison@001', max_output_tokens=500, temperature=0.7)
+
+        return llm(prompt)
 
 
     
