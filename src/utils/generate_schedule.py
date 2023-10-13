a60c718b1591fb4caccabe456a1955bc9a882951
Chua Chia Chiat <chia_chiat_chua@aiap.sg>
2023-10-13 13:06:33 +0800

    generate schedule returns a list temporary

---
+++ src/utils/generate_schedule.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+
+
+
+
+class Generate_Schedule:
+
+    """
+    Class to return schedule
+    todo : generate schedule
+    
+    
+    """
+
+    def __init__(self):
+        pass
+
+
+    def fixed_schedule(self):
+              
+        observations = [
+        "Joe wakes up to the sound of a noisy construction site outside his window.",
+        "Joe gets out of bed and heads to the kitchen to make himself some coffee.",
+        "Joe realizes he forgot to buy coffee filters and starts rummaging through his moving boxes to find some.",
+        "Joe finally finds the filters and makes himself a cup of coffee.",
+        "The coffee tastes bitter, and Joe regrets not buying a better brand.",
+        "Joe checks his email and sees that he has no job offers yet.",
+        "Joe spends some time updating his resume and cover letter.",
+        "Joe heads out to explore the city and look for job openings.",
+        "Joe sees a sign for a job fair and decides to attend.",
+        "The line to get in is long, and Joe has to wait for an hour.",
+        "Joe meets several potential employers at the job fair but doesn't receive any offers.",
+        "Joe leaves the job fair feeling disappointed.",
+        "Joe stops by a local diner to grab some lunch.",
+        "The service is slow, and Joe has to wait for 30 minutes to get his food.",
+        "Joe overhears a conversation at the next table about a job opening.",
+        "Joe asks the diners about the job opening and gets some information about the company.",
+        "Joe decides to apply for the job and sends his resume and cover letter.",
+        "Joe continues his search for job openings and drops off his resume at several local businesses.",
+        "Joe takes a break from his job search to go for a walk in a nearby park.",
+        "A dog approaches and licks Joe's feet, and he pets it for a few minutes.",
+        "Joe sees a group of people playing frisbee and decides to join in.",
+        "Joe has fun playing frisbee but gets hit in the face with the frisbee and hurts his nose.",
+        "Joe goes back to his apartment to rest for a bit.",
+        "A raccoon tore open the trash bag outside his apartment, and the garbage is all over the floor.",
+        "Joe starts to feel frustrated with his job search.",
+        "Joe calls his best friend to vent about his struggles.",
+        "Joe's friend offers some words of encouragement and tells him to keep trying.",
+        "Joe feels slightly better after talking to his friend.",
+        ]
+
+        return(observations)
+
+
+    
+
