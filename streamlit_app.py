8c39f73d6554d52f6396eb24e9078b3098b8dab6
Augustine Teo XZ <augustine_teo@aiap.sg>
2023-10-15 01:16:41 +0800

    Finalized agents page, added function for printing console

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
-import argparse
 import os
+import sys
+from contextlib import contextmanager
 from csv import writer
+from io import StringIO
+from threading import current_thread
 from typing import List
 
 import streamlit as st
@@ -9,9 +12,8 @@ from langchain.document_loaders import CSVLoader
 from langchain.embeddings import VertexAIEmbeddings
 from langchain.llms import VertexAI
 from langchain.schema import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from streamlit.runtime.scriptrunner.script_run_context import SCRIPT_RUN_CONTEXT_ATTR_NAME
 from streamlit_option_menu import option_menu
-from vertexai.preview.language_models import TextGenerationModel
 
 from src.generative_agents.generative_agent import StemssGenerativeAgent
 from src.generative_agents.memory import StemssGenerativeAgentMemory
@@ -19,6 +21,40 @@ from src.generators.agent import generate_agent_name, generate_characters
 from src.generators.schedule import generate_schedule
 from src.retrievers.time_weighted_retriever import ModTimeWeightedVectorStoreRetriever
 from src.vectorstores.chroma import EnhancedChroma
+
+
+@contextmanager
+def st_redirect(src, dst):
+    placeholder = st.empty()
+    output_func = getattr(placeholder, dst)
+
+    with StringIO() as buffer:
+        old_write = src.write
+
+        def new_write(b):
+            if getattr(current_thread(), SCRIPT_RUN_CONTEXT_ATTR_NAME, None):
+                buffer.write(b)
+                output_func(buffer.getvalue())
+            else:
+                old_write(b)
+
+        try:
+            src.write = new_write
+            yield
+        finally:
+            src.write = old_write
+
+
+@contextmanager
+def st_stdout(dst):
+    with st_redirect(sys.stdout, dst):
+        yield
+
+
+@contextmanager
+def st_stderr(dst):
+    with st_redirect(sys.stderr, dst):
+        yield
 
 
 def load_documents() -> List[Document]:
@@ -57,20 +93,20 @@ if __name__ == "__main__":
     if "initalized" not in st.session_state:
         # Load the .env file
         load_dotenv()
-
         # Get the path of Google Cloud credentials from the environment variable
         google_creds_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-
-        # Optionally: Ensure that the variable has been set correctly
+        # Ensure that the variable has been set correctly
         if google_creds_path is None:
             raise ValueError("GOOGLE_APPLICATION_CREDENTIALS is not set in the .env file")
         else:
             print(f"GOOGLE_APPLICATION_CREDENTIALS set to {google_creds_path}")
-
-        st.session_state["llm"] = VertexAI(
+        # Instantiate and store llm to session state
+        llm = VertexAI(
             model_name="text-bison@001", max_output_tokens=256, temperature=0.2
         )
+        st.session_state["llm"] = llm
 
+        # Location of agent's memory from each run
         path = "./outputs"
         all_folders = os.listdir(path)
         new = 1
@@ -94,11 +130,10 @@ if __name__ == "__main__":
             [
                 "Home",
                 "Settings",
-                "Initialize Agents",
                 "Agents and Controls",
                 "View Detailed Logs",
             ],
-            icons=["house", "gear", "person", "chat-left-quote", "search"],
+            icons=["house", "gear", "person", "search"],
             menu_icon="list",
             default_index=0,
         )
@@ -116,89 +151,128 @@ if __name__ == "__main__":
     if st.session_state["selected_option"] == "Settings":
         st.title("All settings")
         st.header("Retriever related settings")
-        st.session_state["decay_rate"] = st.slider("Decay Rate", 0.0, 1.0, 0.05)
-        st.session_state["top_k"] = st.slider("Top K", 1, 10, 1)
+        st.session_state["decay_rate"] = st.slider("Decay Rate", 0.0, 1.0, 0.2, 0.05)
+        st.session_state["top_k"] = st.slider("Top K", 1, 10, 5, 1)
 
-    if st.session_state["selected_option"] == "Initialize Agents":
-        st.header("Agent Initialization")
+    if st.session_state["selected_option"] == "Agents and Controls":
+        st.header("Agents Initialization")
+        st.divider()
         st.write("Agent names are generated randomly")
-        if st.button("Generate Agent Names"):
-            agent_names = generate_agent_name(
-                model=st.session_state["llm"], num_of_agents=2
-            )
-            st.session_state["agent_names"] = agent_names
-            st.write(f"Agent names are {agent_names[0]} and {agent_names[1]}")
 
-            st.write("Generating agents")
-            agents = []
-            for agent_name in agent_names:
-                # generate agent details
-                agent_details = generate_characters(
-                    model=st.session_state["llm"], agent_name=agent_name
+        set_bool = st.checkbox("Retriever settings set?")
+        if set_bool:
+            if st.button("Generate Agents"):
+                progress = 0
+                my_bar = st.progress(progress, text="Generating agents. Please wait.")
+                agent_names = generate_agent_name(
+                    model=st.session_state["llm"], num_of_agents=2
                 )
-                agent_details["name"] = agent_name
+                st.session_state["agent_names"] = agent_names
+                st.write(f"Agent names are {agent_names[0]} and {agent_names[1]}")
 
-                # Create csv if it doesn't exist
-                new_path = st.session_state["new_path"]
-                mem_file = f"{new_path}/memory/{agent_name}.csv"
-                if not os.path.exists(mem_file):
-                    with open(mem_file, "a") as f_object:
-                        List = [
-                            "created_at",
-                            "last_accessed_at",
-                            "observations",
-                            "importance",
-                        ]
-                        writer_object = writer(f_object)
-                        writer_object.writerow(List)
-                        f_object.close()
-
-                # Load CSV
-                docs = load_documents()
-                memory_retriever = create_new_memory_retriever(
-                    decay_rate=st.session_state["decay_rate"],
-                    k=st.session_state["top_k"],
-                    mem_file=mem_file,
-                )
-                if len(docs) != 0:
-                    memory_retriever.add_documents(docs)
-
-                agent_memory = StemssGenerativeAgentMemory(
-                    llm=st.session_state["llm"],
-                    memory_retriever=memory_retriever,
-                    reflection_threshold=8,  # we will give this a relatively low number to show how reflection works
-                    verbose=True,
+                progress += 10
+                my_bar.progress(
+                    progress,
+                    text="Agent's name generated, generating character now...",
                 )
 
-                agent = StemssGenerativeAgent(
-                    name=agent_name,
-                    age=agent_details["age"],
-                    traits=agent_details["traits"],
-                    status="",
-                    memory_retriever=create_new_memory_retriever(),
-                    llm=st.session_state["llm"],
-                    memory=agent_memory,
-                    background=agent_details["background"],
-                )
-                # agent.schedule = generate_schedule(model=llm, agent=agent)
-                observations = generate_schedule(
-                    model=st.session_state["llm"], agent=agent
-                )
-                for observation in observations:
-                    agent.memory.add_memory(observation)
-                agents.append(agent)
+                agents = []
+                for idx, agent_name in enumerate(agent_names):
+                    # Generate agent details
+                    agent_details = generate_characters(
+                        model=st.session_state["llm"], agent_name=agent_name
+                    )
+                    agent_details["name"] = agent_name
+
+                    progress += 15
+                    my_bar.progress(
+                        progress, text=f"Generated personality for {agent_name}"
+                    )
+
+                    # Create csv if it doesn't exist
+                    new_path = st.session_state["new_path"]
+                    mem_file = f"{new_path}/memory/{agent_name}.csv"
+                    if not os.path.exists(mem_file):
+                        with open(mem_file, "a") as f_object:
+                            List = [
+                                "created_at",
+                                "last_accessed_at",
+                                "observations",
+                                "importance",
+                            ]
+                            writer_object = writer(f_object)
+                            writer_object.writerow(List)
+                            f_object.close()
+
+                    # Load CSV
+                    docs = load_documents()  # currently loads nothing
+                    memory_retriever = create_new_memory_retriever(
+                        decay_rate=st.session_state["decay_rate"],
+                        k=st.session_state["top_k"],
+                        mem_file=mem_file,
+                    )
+                    if len(docs) != 0:
+                        memory_retriever.add_documents(docs)
+
+                    # Instantiate agent's memory
+                    agent_memory = StemssGenerativeAgentMemory(
+                        llm=st.session_state["llm"],
+                        memory_retriever=memory_retriever,
+                        reflection_threshold=8,  # we will give this a relatively low number to show how reflection works
+                        verbose=True,
+                    )
+                    # Instantiate agent
+                    agent_name = agent_details["name"]
+                    agent = StemssGenerativeAgent(
+                        name=agent_name,
+                        age=agent_details["age"],
+                        traits=agent_details["traits"],
+                        status="",
+                        memory_retriever=create_new_memory_retriever(),
+                        llm=st.session_state["llm"],
+                        memory=agent_memory,
+                        background=agent_details["background"],
+                    )
+                    # Get each agent's summary
+                    agent.get_summary()
+                    # Store each agent to a list
+                    agents.append(agent)
+
+                    progress += 15
+                    my_bar.progress(progress, text=f"Generated agent {agent_name}")
+                # Store agents into session_state
                 st.session_state["agents"] = agents
 
-                st.write("Successfully generated agents")
-                for agent in agents:
-                    st.info(agent.get_summary())
-        # st.radio("Select the agent you would like to initialize", agent_names)
-        # mem_file = st.file_uploader("Upload memory files")
-    if st.session_state["selected_option"] == "Agents and Controls":
-        st.info(st.session_state["agent_names"])
+                for single_agent in agents:
+                    observations = generate_schedule(
+                        model=st.session_state["llm"], agent=single_agent
+                    )
+                    progress += 10
+                    my_bar.progress(
+                        progress, text=f"Adding memories for {single_agent.name}"
+                    )
+                    for observation in observations:
+                        single_agent.memory.add_memory(observation)
 
-        x = st.text_area("Write the memory you would like to inject")
-        # st.button("Inject memories", on_click=pass)
+                for single_agent in agents:
+                    print(single_agent.get_summary())
+                    print("\n")
+                progress += 10
+                my_bar.progress(progress, text="Completed")
+
+                st.success("Successfully generated all agents")
+                # To resolve the list index out of range error
+                memory_retriever.vectorstore.delete_collection()
+
+        st.header("Control Agents")
+        st.divider()
+        st.write("Inject Memories")
+
+    # if st.session_state["selected_option"] == "Agents and Controls":
+    # st.info(st.session_state["agent_names"])
+
+    # x = st.text_area("Write the memory you would like to inject")
+    # st.button("Inject memories", on_click=pass)
 
     if st.session_state["selected_option"] == "View Interactions":
         pass
