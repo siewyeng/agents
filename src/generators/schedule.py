c69b80b7689a691c34809e9f2e4d638809ff7488
Chua Chia Chiat <chia_chiat_chua@aiap.sg>
2023-10-15 09:34:58 +0800

    Mod a bit using prompt template

--- src/generators/schedule.py
+++ src/generators/schedule.py
@@ -1,4 +1,7 @@
+from langchain.prompts import PromptTemplate
 from src.generative_agents.generative_agent import StemssGenerativeAgent
+
+
 def generate_schedule(model,agent):
         """
         Generate the schedule for the agent base on the bio provided
@@ -12,11 +15,15 @@ def generate_schedule(model,agent):
             [list]: [Schedule generated from LLM]
         """
 
-
-
-        prompt =f"You are a plan generating AI, and your job is to help characters make new plans based on new information. Given the character's info(bio, goals), generate a new set of plans of the day for them to carry out, such that the final set of plans and include at least 15 individual plans. The plan list should be numbered in the order in which they should be performed, with each plan containing a description with at least 10 words and never use the word /'repeat/'. Give output in this format without numbering: [Plan to take dinner with their spouse at home,Plan to thave a jog around town] Try to collaborate with spouse if possible. The location can only be office or home. Do not mention about repeating or evaluating the plan. Always prioritize finishing any pending conversation before doing other things. Let\'s Begin! Name: {agent.name} Age: {agent.age} Background: {agent.background} "
+        template = """You are a plan generating AI, and your job is to help characters make new plans based on new information. Given the character's info(bio, goals), generate a new set of plans of the day for them to carry out, such that the final set of plans and include at least 15 individual plans. The plan list should be numbered in the order in which they should be performed, with each plan containing a description with at least 10 words and never use the word /'repeat/'. Give output in this format without numbering: [Plan to take dinner with their spouse at home, Plan to thave a jog around town]. Try to collaborate with spouse if possible. The location can only be office or home. Do not mention about repeating or evaluating the plan. Always prioritize finishing any pending conversation before doing other things. Let\'s Begin! Name: {name} Age: {age} Background: {background}"""
         
-        schedule = model(prompt)
+        prompt_template = PromptTemplate(
+                input_variables=["name","age","background"],
+                template=template
+                )
+        
+        schedule = model(prompt_template.format(name=agent.name, age=agent.age, background=agent.background))
         schedule = schedule.replace("[","").replace("]","")
         schedule = schedule.split(",")
         return schedule
+
