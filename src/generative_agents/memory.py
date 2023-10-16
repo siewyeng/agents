7c6576b95b4bfb15263fe5e466107bff11491589
Foo Chuan Yue <foochuanyue@gmail.com>
2023-10-16 15:18:18 +0800

    end conversation consistently

--- src/generative_agents/memory.py
+++ src/generative_agents/memory.py
@@ -29,11 +29,35 @@ class StemssGenerativeAgentMemory(GenerativeAgentMemory):
             + " extremely important (e.g., a break up, college"
             + " acceptance), rate the importance of the"
             + " following piece of memory. You must respond with a single integer."
+            + "Give an explanation for your response."
             "\nMemory: {memory_content}"
             "\nRating: "
+            "\nExplanation: "
         )
         prompt = PromptTemplate.from_template(template)
-
+        score = self.chain(prompt).run(memory_content=memory_content).strip()
+        if self.verbose:
+            logger.info(f"Importance score: {score}")
+        match = re.search(r"^\D*(\d+)", score)
+        if match:
+            return (float(match.group(1)) / 10) * self.importance_weight
+        else:
+            return 0.0
+    
+    def _score_memory_importance_with_explanation(self, memory_content: str) -> float:
+        """Score the absolute importance of the given memory."""
+        template = (
+            "On the scale of 1 to 10, where 1 is not important at all"
+            + " (e.g., brushing teeth, making bed) and 10 is"
+            + " extremely important (e.g., a break up, college"
+            + " acceptance), rate the importance of the"
+            + " following piece of memory. You must respond with a single integer."
+            + "Give an explanation for your response."
+            "\nMemory: {memory_content}"
+            "\nRating: "
+            "\nExplanation: "
+        )
+        prompt = PromptTemplate.from_template(template)
         score = self.chain(prompt).run(memory_content=memory_content).strip()
         if self.verbose:
             logger.info(f"Importance score: {score}")
@@ -55,9 +79,3 @@ class StemssGenerativeAgentMemory(GenerativeAgentMemory):
             )
             content.append(f"- {created_time}: {mem.page_content.strip()}")
         return "\n".join([f"{mem}" for mem in content])
-
-    def addToCSV():
-        with open("event.csv", "a") as f_object:
-            writer_object = writer(f_object)
-            writer_object.writerow(List)
-            f_object.close()
